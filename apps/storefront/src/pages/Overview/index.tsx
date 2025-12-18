7fcb01ba1d44ccb368d17f5ef346ec1465ee417d
Chris Nanninga <chris.nanninga@bigcommerce.com>
2025-12-18 15:25:28 -0600

    Add router configuration for Overview route

---
+++ apps/storefront/src/pages/Overview/index.tsx
@@ -0,0 +1,14 @@
+import {
+  Card,
+} from "@mui/material";
+
+export default function Overview() {
+
+  return (
+    <>
+      <Card sx={{ padding: '20px' }}>
+        <h3>Hello World!</h3>
+      </Card>
+    </>
+  );
+}
