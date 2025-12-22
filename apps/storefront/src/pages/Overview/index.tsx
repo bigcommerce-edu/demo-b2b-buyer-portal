822838a698f8f6824eb7a40ea60f95632f6c3f6f
Chris Nanninga <chris.nanninga@bigcommerce.com>
2025-12-22 16:04:31 -0600

    Navigation buttons on the Overview page

--- apps/storefront/src/pages/Overview/index.tsx
+++ apps/storefront/src/pages/Overview/index.tsx
@@ -1,24 +1,63 @@
 import {
+  Box,
+  Button, 
   Card,
 } from "@mui/material";
+import { type SetOpenPage } from '@/pages/SetOpenPage';
+import { HeadlessRoutes } from "@/constants";
 
-// TODO: Add `OverviewProps` interface
-//  - `setOpenPage` should be of the type `SetOpenPage`
+interface OverviewProps {
+  setOpenPage: SetOpenPage;
+}
 
-// TODO: Update the component signature to accept props and destructure 
-export default function Overview() {
-  // TODO: Use `HeadlessRoutes` destructuring to get the constants for `ORDERS`, `INVOICE`, and `QUOTES`
+export default function Overview({
+  setOpenPage,
+}: OverviewProps) {
+  const { ORDERS, QUOTES, SHOPPING_LISTS } = HeadlessRoutes;
 
   return (
     <>
       <Card sx={{ padding: '20px' }}>
         <h3>Hello World!</h3>
 
-        {/* TODO: Add a simple Box with navigation buttons
-              - Use the `Box` and `Button` components from Material UI
-              - Use the 'contained' variant for each button
-              - Each button should have an `onClick` to use `setOpenPage` with the appropriate route string
-        */}
+        <Box sx={{ textAlign: 'center' }}>
+          <Button
+            onClick={() => {
+              setOpenPage({
+                isOpen: true,
+                openUrl: ORDERS,
+              });
+            }}
+            sx={{ margin: '10px' }}
+            variant="contained"
+          >
+            Orders
+          </Button>
+          <Button
+            onClick={() => {
+              setOpenPage({
+                isOpen: true,
+                openUrl: QUOTES,
+              });
+            }}
+            sx={{ margin: '10px' }}
+            variant="contained"
+          >
+            Quotes
+          </Button>
+          <Button
+            onClick={() => {
+              setOpenPage({
+                isOpen: true,
+                openUrl: SHOPPING_LISTS,
+              });
+            }}
+            sx={{ margin: '10px' }}
+            variant="contained"
+          >
+            Shopping Lists
+          </Button>
+        </Box>
       </Card>
     </>
   );
